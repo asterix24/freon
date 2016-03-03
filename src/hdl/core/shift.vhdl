cdc0205c36f95a56c7eead4d86245b1d9bc9bb4c
Daniele Basile <asterix@develer.com>
2016-03-03 15:00:01 +0100

    Add shifter left code. Add testbench code.

--- src/hdl/core/shift.vhdl
+++ src/hdl/core/shift.vhdl
@@ -1,39 +1,32 @@
--- Design: shift instruction
+-- Design:
+--	Shifter unit for the Freon core.
+--
+-- Authors:
+--	Daniele Basiel <asterix24@gmail.com>
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity shift is
-  generic (
-            SLEN : integer := 5;
-            DATA : integer := 32
-          );
-  port (
-         rst      : in std_logic;
-         shamt    : in std_logic_vector(4 downto 0);
-         data_out : out std_logic_vector(DATA-1 downto 0);
-         data_in  : in std_logic_vector(DATA-1 downto 0)
-       );
+	generic (
+			DATA : integer := 32
+		);
+	port (
+		     shamt    : in std_logic_vector(4 downto 0);
+		     data_out : out std_logic_vector(DATA-1 downto 0);
+		     data_in  : in std_logic_vector(DATA-1 downto 0)
+	     );
 end entity;
 
 architecture beh of shift is
-  signal tmp : std_logic_vector(32 downto 0);
+	signal s0, s1, s2, s3, s4 : std_logic_vector(DATA-1 downto 0);
 begin
-  process(rst, shamt, data_in)
-  begin
-    if (rst = '1') then
-      data_out <= (others => 'Z');
-    end if;
+	s0 <= data_in(30 downto 0) & '0' when shamt(0) = '1' else data_in;
+	s1 <= s0(29 downto 0) & "00"     when shamt(1) = '1' else s0;
+	s2 <= s1(27 downto 0) & X"0"     when shamt(2) = '1' else s1;
+	s3 <= s2(23 downto 0) & X"00"    when shamt(3) = '1' else s2;
+	s4 <= s3(15 downto 0) & X"0000"  when shamt(4) = '1' else s3;
 
-    case shamt is
-      when "00000" =>
-        data_out <= data_in;
-      when "00001" =>
-        for i in 32 downto 0 loop
-          data_out(i+1) <= data_in(i);
-        end loop;
-      when others =>
-        data_out <= (others => 'X');
-    end case;
-  end process;
+	data_out <= s4;
 end architecture;
--- tests/hdl/core/shift_tb.vhdl
+++ tests/hdl/core/shift_tb.vhdl
@@ -1,4 +1,8 @@
--- Design: Testbench for the shift
+-- Design:
+--	Testbench for the Shifter file of the Freon core.
+--
+-- Authors:
+--	Daniele Basiel <asterix24@gmail.com>
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -10,7 +14,6 @@ end entity; -- shift_tb
 architecture tb of shift_tb is
   constant T : time := 1 ns; -- arbitrary test time
 
-  signal rst : std_logic := '0';
   signal shamt : std_logic_vector(4 downto 0) := (others => '0');
   signal data_in : std_logic_vector(31 downto 0) := (others => '0');
   signal data_out : std_logic_vector(31 downto 0) := (others => '0');
@@ -18,7 +21,6 @@ begin
   -- unit under test
   uut : entity work.shift
   port map (
-    rst => rst,
     shamt => shamt,
     data_in => data_in,
     data_out => data_out
@@ -27,28 +29,71 @@ begin
 -- testbench process
 tb_proc : process
   begin
-    rst <= '1';
-    wait for T;
 
-    rst <= '0';
+	data_in <= X"00000001"; shamt <= "00000"; -- 0
     wait for T;
+	assert data_out <= X"00000001" report "[SLL] Wrong result" severity failure;
 
-    data_in <= X"00000001"; shamt <= "00000";
+	data_in <= X"00000001"; shamt <= "00001"; -- 1
     wait for T;
-    assert data_out = X"00000001" report "Wrong result" severity failure;
+	assert data_out <= X"00000002" report "[SLL] Wrong result" severity failure;
 
-    data_in <= X"00000002"; shamt <= "00001";
+	data_in <= X"00000001"; shamt <= "00111"; -- 7
     wait for T;
-    assert data_out = X"00000001" report "Wrong result" severity failure;
+	assert data_out <= X"00000080" report "[SLL] Wrong result" severity failure;
 
-    --0x00000001, 0x00000001, 0  );
-    --0x00000002, 0x00000001, 1  );
-    --0x00000080, 0x00000001, 7  );
-    --0x00004000, 0x00000001, 14 );
-    --0x80000000, 0x00000001, 31 );
-
+	data_in <= X"00000001"; shamt <= "01110"; -- 14
     wait for T;
-      assert false report "Simulation over" severity failure;
+	assert data_out <= X"00004000" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"00000001"; shamt <= "11111"; -- 31
+    wait for T;
+	assert data_out <= X"80000000" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"ffffffff"; shamt <= "00000"; -- 0
+    wait for T;
+	assert data_out <= X"ffffffff" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"ffffffff"; shamt <= "00001"; -- 1
+    wait for T;
+	assert data_out <= X"fffffffe" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"ffffffff"; shamt <= "00111"; -- 7
+    wait for T;
+	assert data_out <= X"ffffff80" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"ffffffff"; shamt <= "01110"; -- 14
+    wait for T;
+	assert data_out <= X"ffffc000" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"ffffffff"; shamt <= "11111"; -- 31
+    wait for T;
+	assert data_out <= X"80000000" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"21212121"; shamt <= "00000"; -- 0
+    wait for T;
+	assert data_out <= X"21212121" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"21212121"; shamt <= "00001"; -- 1
+    wait for T;
+	assert data_out <= X"42424242" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"21212121"; shamt <= "00111"; -- 7
+    wait for T;
+	assert data_out <= X"90909080" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"21212121"; shamt <= "01110"; -- 14
+    wait for T;
+	assert data_out <= X"48484000" report "[SLL] Wrong result" severity failure;
+
+	data_in <= X"21212121"; shamt <= "11111"; -- 31
+    wait for T;
+	assert data_out <= X"80000000" report "[SLL] Wrong result" severity failure;
+
+
+
+
+	wait; -- Terminate testbench
   end process; -- tb_proc
 end architecture; -- tb
 
